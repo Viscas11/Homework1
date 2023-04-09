2e24bb7fb26103a83622737090a25f9e0e3d580d
Syat <titmouse99@mail.ru>
2023-04-09 23:45:53 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Мир");
-        var dog = 8 ;
-        var cat = 6 ;
-        var paper = 763789 ;
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
+        var dog = 8.00 ;
+        var cat = 6.00 ;
+        var paper = 763789.00 ;
+         dog = dog + 4;
+         cat = cat + 4;
+         paper = paper + 4;
+         dog = dog - 3.5;
+         cat = cat + 1.6;
+         paper = paper + 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-
     }
 }
