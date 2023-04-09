db1bb07bd22e42895d7dbdfcad2ceda18e5daaa5
Syat <titmouse99@mail.ru>
2023-04-09 23:58:33 +0300

    5 задача

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир");
-      var frog = 3.5;
-        System.out.println(frog);
-      frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+    var first_boxer = 78.2;
+    var second_boxer = 82.7;
+        System.out.println(first_boxer + second_boxer);
     }
 }
